5d81491b935f535784baa2f34e4e788e2b2eb52e
daasc <paulojhol@gmail.com>
2022-04-06 18:29:50 -0300

    add play mutation and add your test

--- store/hash.js
+++ store/hash.js
@@ -1,6 +1,16 @@
-export const state = () => ({})
+export const state = () => ({
+  hash: {
+    x: [],
+    y: [],
+    z: [],
+  },
+})
 
-export const mutations = {}
+export const mutations = {
+  PLAY(state, payload) {
+    state.hash[payload.move][payload.index] = payload.value
+  },
+}
 
 export const getters = {}
 
