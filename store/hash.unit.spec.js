c7069721ffde1b06ceaa246e0d9330e9c1da6336
daasc <paulojhol@gmail.com>
2022-04-07 18:06:53 -0300

    add check play

--- store/hash.unit.spec.js
+++ store/hash.unit.spec.js
@@ -19,7 +19,7 @@ describe('Hash', () => {
     const store = new Vuex.Store(storeConfig)
     return { store }
   }
-  it('should return the value ', () => {
+  it('should return the value hash', () => {
     const { store } = createStore()
     expect(store.state.hash).toEqual({
       x: [],
@@ -27,9 +27,42 @@ describe('Hash', () => {
       z: [],
     })
   })
+  it('should return the value result', () => {
+    const { store } = createStore()
+    expect(store.state.result).toEqual(null)
+  })
   it('should make a play on the hash when PLAY is called', () => {
     const { store } = createStore()
     store.commit('PLAY', { move: 'x', index: 1, value: 'X' })
     expect(store.state.hash.x[1]).toBe('X')
+  })
+  it('should to be false the value of the result when CHECK_PLAY is called', () => {
+    const { store } = createStore()
+    store.commit('PLAY', { move: 'x', index: 0, value: 'x' })
+    store.commit('PLAY', { move: 'x', index: 1, value: '0' })
+    store.commit('PLAY', { move: 'x', index: 2, value: 'x' })
+    store.commit('PLAY', { move: 'y', index: 0, value: '0' })
+    store.commit('PLAY', { move: 'y', index: 1, value: '0' })
+    store.commit('PLAY', { move: 'y', index: 2, value: 'x' })
+    store.commit('PLAY', { move: 'z', index: 0, value: 'x' })
+    store.commit('PLAY', { move: 'z', index: 1, value: 'x' })
+    store.commit('PLAY', { move: 'z', index: 2, value: '0' })
+    store.commit('CHECK_PLAY')
+    expect(store.state.result).toBe(false)
+  })
+
+  it('should to be tue the value of the result when CHECK_PLAY is called', () => {
+    const { store } = createStore()
+    store.commit('PLAY', { move: 'x', index: 0, value: 'x' })
+    store.commit('PLAY', { move: 'x', index: 1, value: 'x' })
+    store.commit('PLAY', { move: 'x', index: 2, value: 'x' })
+    store.commit('PLAY', { move: 'y', index: 0, value: '0' })
+    store.commit('PLAY', { move: 'y', index: 1, value: '0' })
+    store.commit('PLAY', { move: 'y', index: 2, value: 'x' })
+    store.commit('PLAY', { move: 'z', index: 0, value: 'x' })
+    store.commit('PLAY', { move: 'z', index: 1, value: 'x' })
+    store.commit('PLAY', { move: 'z', index: 2, value: '0' })
+    store.commit('CHECK_PLAY')
+    expect(store.state.result).toBe(true)
   })
 })
