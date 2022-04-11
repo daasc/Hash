6ba51f90517dc5d86df267acd6217fc9b2a41216
daasc <paulojhol@gmail.com>
2022-04-11 14:06:36 -0300

    add style scoreboard

--- components/ScoreBoard.vue
+++ components/ScoreBoard.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="scoreboard">
-    <div date-testid="player_0">Player 0: {{ scoreboard.player_0 }}</div>
-    <div date-testid="player_x">Player X: {{ scoreboard.player_x }}</div>
+    <div date-testid="player_0">
+      <span> 0 </span>
+      <span>{{ scoreboard.player_0 }}</span>
+    </div>
+    <div date-testid="player_x">
+      <span>X</span>
+      <span>{{ scoreboard.player_x }}</span>
+    </div>
   </div>
 </template>
 <script>
@@ -15,10 +21,48 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.scoreboard {
-  display: flex;
-  width: 40%;
-  justify-content: space-between;
-  margin: 0 auto;
+@media (min-width: 0px) {
+  .scoreboard {
+    display: flex;
+    width: 60%;
+    justify-content: space-between;
+    margin: 0 auto;
+    margin-bottom: 50px;
+    background-color: #202124;
+    padding: 10px;
+    // #
+
+    div {
+      width: 40%;
+      display: flex;
+      justify-content: space-between;
+      padding: 5px;
+      border-top: 1px solid;
+      border-right: 1px solid;
+      border-bottom: 3.5px solid;
+      border-left: 1px solid;
+      border-color: #17bdac;
+      border-radius: 6px;
+
+      span:first-child {
+        color: white;
+      }
+      span:last-child {
+        color: #8d9398;
+      }
+    }
+  }
+}
+
+@media (min-width: 1200px) {
+  .scoreboard {
+    display: flex;
+    width: 30%;
+    justify-content: space-between;
+    margin: 0 auto;
+    margin-bottom: 100px;
+    background-color: #202124;
+    padding: 10px;
+  }
 }
 </style>
